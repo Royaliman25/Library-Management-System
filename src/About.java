ca81ef4d63de865d979373cc3d51944daa212e0f
Fitri <38976894+FitrianiGitana3@users.noreply.github.com>
2020-06-27 21:59:46 +0700

    about library managemen system

---
+++ src/About.java
@@ -0,0 +1,11 @@
+//import the packages for using the classes in them into the program
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *A public class
+ */
+public class About extends JPanel {
+	
+}
